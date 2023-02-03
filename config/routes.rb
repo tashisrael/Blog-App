987e473ec8531742acd45c0eb8335e51068bba20
tashisrael <tashisrael04@gmail.com>
2023-02-03 15:00:49 +0300

    Add routes for comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  resources :users, ony: [:index, :show] do 
-    resources :posts,only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create] 
+        resources :likes, only: [:new] 
+    end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
