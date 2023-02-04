4be02c0d09f335f097a26e987dfa5ce2766d75d4
tashisrael <tashisrael04@gmail.com>
2023-02-04 10:36:15 +0300

    review corrections

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,13 +17,13 @@ class PostsController < ApplicationController
 
   def create
     puts params
-    user = current_user
+    author = current_user
     new_post = Post.new(post_params)
-    new_post.author = user
+    new_post.author = author
     if new_post.save
-      redirect_to user_posts_path(id: user.id)
+      redirect_to user_posts_path(id: author.id)
     else
-      redirect_to user_posts_path(user_id: user.id)
+      redirect_to user_posts_path(user_id: author.id)
     end
   end
 
